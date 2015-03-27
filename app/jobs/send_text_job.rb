59cb36464fb661b6f72ecf327d871b3d30ba7cb4
Jane Alexander <jalexanderny@earthlink.net>
2015-03-27 11:34:52 -0400

    Environment Keys

--- app/jobs/send_text_job.rb
+++ app/jobs/send_text_job.rb
@@ -2,13 +2,16 @@ class SendTextJob < ActiveJob::Base
 
 	def perform(medicine)
 		#Get your Account Sid and Auth Token from twilio.com/user/account
+		account_sid = ENV['TWILIO_SID']
+		auth_token = ENV['TWILIO_AUTH']
+		from_number = ENV['TWILIO_NUMBER']
 
 		@client = Twilio::REST::Client.new account_sid, auth_token
   		Medicine.all.each do |med|
 	 		message = @client.account.messages.create(
 	 			:body => body, 
 	     		:to => "medicine.user.telephone_number",
-	    		:from => "+12013409571")
+	    		:from => from_number)
 	 		puts message.to #puts message.sid?
  		end
 	end
